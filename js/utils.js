8edc3cd8fdf0db28da0311e33d8cc851cd278115
ZJZLaptop <xjlzjz@163.com>
2022-08-25 22:54:37 +0800

    Site updated: 2022-08-25 22:54:11

--- js/utils.js
+++ js/utils.js
@@ -1,362 +1,46 @@
-/* global KEEP */
+function debounce(func, wait, immediate) {
+  var timeout
+  return function () {
+    var context = this
+    var args = arguments
+    var later = function () {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+    var callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
+};
 
-KEEP.initUtils = () => {
+function throttle(func, wait, mustRun) {
+  var timeout
+  var startTime = new Date()
 
-  KEEP.utils = {
+  return function () {
+    var context = this
+    var args = arguments
+    var curTime = new Date()
 
-    html_root_dom: document.querySelector('html'),
-    pageContainer_dom: document.querySelector('.page-container'),
-    pageTop_dom: document.querySelector('.page-main-content-top'),
-    firstScreen_dom: document.querySelector('.first-screen-container'),
-    scrollProgressBar_dom: document.querySelector('.scroll-progress-bar'),
-    pjaxProgressBar_dom: document.querySelector('.pjax-progress-bar'),
-    pjaxProgressIcon_dom: document.querySelector('.pjax-progress-icon'),
-    back2TopButton_dom: document.querySelector('.tool-scroll-to-top'),
-
-    innerHeight: window.innerHeight,
-    pjaxProgressBarTimer: null,
-    prevScrollValue: 0,
-    fontSizeLevel: 0,
-
-    isHasScrollProgressBar: KEEP.theme_config.style.scroll.progress_bar.enable === true,
-    isHasScrollPercent: KEEP.theme_config.style.scroll.percent.enable === true,
-
-    // Scroll Style Handle
-    styleHandleWhenScroll() {
-      const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-      const scrollHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
-      const clientHeight = window.innerHeight || document.documentElement.clientHeight;
-
-      const percent = Math.round(scrollTop / (scrollHeight - clientHeight) * 100);
-
-      if (this.isHasScrollProgressBar) {
-        const ProgressPercent = (scrollTop / (scrollHeight - clientHeight) * 100).toFixed(3);
-        this.scrollProgressBar_dom.style.visibility = percent === 0 ? 'hidden' : 'visible';
-        this.scrollProgressBar_dom.style.width = `${ProgressPercent}%`;
-      }
-
-      if (this.isHasScrollPercent) {
-        const percent_dom = this.back2TopButton_dom.querySelector('.percent');
-        if (percent === 0 || percent === undefined) {
-          this.back2TopButton_dom.classList.remove('show');
-
-        } else {
-          this.back2TopButton_dom.classList.add('show');
-          percent_dom.innerHTML = percent.toFixed(0);
-        }
-      }
-
-      // hide header handle
-      if (scrollTop > this.prevScrollValue && scrollTop > this.innerHeight) {
-        this.pageTop_dom.classList.add('hide');
-      } else {
-        this.pageTop_dom.classList.remove('hide');
-      }
-      this.prevScrollValue = scrollTop;
-    },
-
-    // register window scroll event
-    registerWindowScroll() {
-      window.addEventListener('scroll', () => {
-        // style handle when scroll
-        if (this.isHasScrollPercent || this.isHasScrollProgressBar) {
-          this.styleHandleWhenScroll();
-        }
-
-        // TOC scroll handle
-        if (KEEP.theme_config.toc.enable && KEEP.utils.hasOwnProperty('findActiveIndexByTOC')) {
-          KEEP.utils.findActiveIndexByTOC();
-        }
-
-        // header shrink
-        KEEP.utils.headerShrink.headerShrink();
-      });
-    },
-
-    // toggle show tools list
-    toggleShowToolsList() {
-      document.querySelector('.tool-toggle-show').addEventListener('click', () => {
-        document.querySelector('.side-tools-list').classList.toggle('show');
-      });
-    },
-
-    // global font adjust
-    globalFontAdjust() {
-      const fontSize = document.defaultView.getComputedStyle(document.body).fontSize;
-      const fs = parseFloat(fontSize);
-
-      const initFontSize = () => {
-        const styleStatus = KEEP.getStyleStatus();
-        if (styleStatus) {
-          this.fontSizeLevel = styleStatus.fontSizeLevel;
-          setFontSize(this.fontSizeLevel);
-        }
-      }
-
-      const setFontSize = (fontSizeLevel) => {
-        this.html_root_dom.style.fontSize = `${fs * (1 + fontSizeLevel * 0.05)}px`;
-        KEEP.styleStatus.fontSizeLevel = fontSizeLevel;
-        KEEP.setStyleStatus();
-      }
-
-      initFontSize();
-
-      document.querySelector('.tool-font-adjust-plus').addEventListener('click', () => {
-        if (this.fontSizeLevel === 5) return;
-        this.fontSizeLevel++;
-        setFontSize(this.fontSizeLevel);
-      });
-
-      document.querySelector('.tool-font-adjust-minus').addEventListener('click', () => {
-        if (this.fontSizeLevel <= 0) return;
-        this.fontSizeLevel--;
-        setFontSize(this.fontSizeLevel);
-      });
-    },
-
-    // toggle content area width
-    contentAreaWidthAdjust() {
-      const toolExpandDom = document.querySelector('.tool-expand-width');
-      const headerContentDom = document.querySelector('.header-content');
-      const mainContentDom = document.querySelector('.main-content');
-      const iconDom = toolExpandDom.querySelector('i');
-
-      const defaultMaxWidth = KEEP.theme_config.style.content_max_width || '1000px';
-      const expandMaxWidth = '90%';
-      let headerMaxWidth = defaultMaxWidth;
-
-      let isExpand = false;
-
-      if (KEEP.theme_config.style.first_screen.enable === true && window.location.pathname === '/') {
-        headerMaxWidth = parseInt(defaultMaxWidth) * 1.2 + 'px';
-      }
-
-      const setPageWidth = (isExpand) => {
-        KEEP.styleStatus.isExpandPageWidth = isExpand;
-        KEEP.setStyleStatus();
-        if (isExpand) {
-          iconDom.classList.remove('fa-arrows-alt-h');
-          iconDom.classList.add('fa-compress-arrows-alt');
-          headerContentDom.style.maxWidth = expandMaxWidth;
-          mainContentDom.style.maxWidth = expandMaxWidth;
-        } else {
-          iconDom.classList.remove('fa-compress-arrows-alt');
-          iconDom.classList.add('fa-arrows-alt-h');
-          headerContentDom.style.maxWidth = headerMaxWidth;
-          mainContentDom.style.maxWidth = defaultMaxWidth;
-        }
-      }
-
-      const initPageWidth = () => {
-        const styleStatus = KEEP.getStyleStatus();
-        if (styleStatus) {
-          isExpand = styleStatus.isExpandPageWidth;
-          setPageWidth(isExpand);
-        }
-      }
-
-      initPageWidth();
-
-      toolExpandDom.addEventListener('click', () => {
-        isExpand = !isExpand;
-        setPageWidth(isExpand)
-      });
-
-
-    },
-
-    // go comment anchor
-    goComment() {
-      this.goComment_dom = document.querySelector('.go-comment');
-      if (this.goComment_dom) {
-        this.goComment_dom.addEventListener('click', () => {
-          document.querySelector('#comment-anchor').scrollIntoView();
-        });
-      }
-
-    },
-
-    // get dom element height
-    getElementHeight(selectors) {
-      const dom = document.querySelector(selectors);
-      return dom ? dom.getBoundingClientRect().height : 0;
-    },
-
-    // init first screen height
-    initFirstScreenHeight() {
-      this.firstScreen_dom && (this.firstScreen_dom.style.height = this.innerHeight + 'px');
-    },
-
-    // init page height handle
-    initPageHeightHandle() {
-      if (this.firstScreen_dom) return;
-      const temp_h1 = this.getElementHeight('.page-main-content-top');
-      const temp_h2 = this.getElementHeight('.page-main-content-middle');
-      const temp_h3 = this.getElementHeight('.page-main-content-bottom');
-      const allDomHeight = temp_h1 + temp_h2 + temp_h3;
-      const innerHeight = window.innerHeight;
-      const pb_dom = document.querySelector('.page-main-content-bottom');
-      if (allDomHeight < innerHeight) {
-        const marginTopValue = Math.floor(innerHeight - allDomHeight);
-        if (marginTopValue > 0) {
-          pb_dom.style.marginTop = `${marginTopValue - 2}px`;
-        }
-      }
-    },
-
-    // big image viewer
-    imageViewer() {
-      let isBigImage = false;
-
-      const showHandle = (maskDom, isShow) => {
-        document.body.style.overflow = isShow ? 'hidden' : 'auto';
-        if (isShow) {
-          maskDom.classList.add('active');
-        } else {
-          maskDom.classList.remove('active');
-        }
-      }
-
-      const imageViewerDom = document.querySelector('.image-viewer-container');
-      const targetImg = document.querySelector('.image-viewer-container img');
-      imageViewerDom && imageViewerDom.addEventListener('click', () => {
-        isBigImage = false;
-        showHandle(imageViewerDom, isBigImage);
-      });
-
-      const imgDoms = document.querySelectorAll('.markdown-body img');
-
-      if (imgDoms.length) {
-        imgDoms.forEach(img => {
-          img.addEventListener('click', () => {
-            isBigImage = true;
-            showHandle(imageViewerDom, isBigImage);
-            targetImg.setAttribute('src', img.getAttribute('src'));
-          });
-        });
-      } else {
-        this.pageContainer_dom.removeChild(imageViewerDom);
-      }
-    },
-
-    // set how long ago language
-    setHowLongAgoLanguage(p1, p2) {
-      return p2.replace(/%s/g, p1)
-    },
-
-    getHowLongAgo(timestamp) {
-      const l = KEEP.language_ago;
-
-      const __Y = Math.floor(timestamp / (60 * 60 * 24 * 30) / 12);
-      const __M = Math.floor(timestamp / (60 * 60 * 24 * 30));
-      const __W = Math.floor(timestamp / (60 * 60 * 24) / 7);
-      const __d = Math.floor(timestamp / (60 * 60 * 24));
-      const __h = Math.floor(timestamp / (60 * 60) % 24);
-      const __m = Math.floor(timestamp / 60 % 60);
-      const __s = Math.floor(timestamp % 60);
-
-      if (__Y > 0) {
-        return this.setHowLongAgoLanguage(__Y, l.year);
-
-      } else if (__M > 0) {
-        return this.setHowLongAgoLanguage(__M, l.month);
-
-      } else if (__W > 0) {
-        return this.setHowLongAgoLanguage(__W, l.week);
-
-      } else if (__d > 0) {
-        return this.setHowLongAgoLanguage(__d, l.day);
-
-      } else if (__h > 0) {
-        return this.setHowLongAgoLanguage(__h, l.hour);
-
-      } else if (__m > 0) {
-        return this.setHowLongAgoLanguage(__m, l.minute);
-
-      } else if (__s > 0) {
-        return this.setHowLongAgoLanguage(__s, l.second);
-      }
-    },
-
-    setHowLongAgoInHome() {
-      const post = document.querySelectorAll('.home-article-meta-info .home-article-date');
-      post && post.forEach(v => {
-        const nowDate = Date.now();
-        const postDate = new Date(v.dataset.date.split(' GMT')[0]).getTime();
-        v.innerHTML = this.getHowLongAgo(Math.floor((nowDate - postDate) / 1000));
-      });
-    },
-
-    // loading progress bar start
-    pjaxProgressBarStart() {
-      this.pjaxProgressBarTimer && clearInterval(this.pjaxProgressBarTimer);
-      if (this.isHasScrollProgressBar) {
-        this.scrollProgressBar_dom.classList.add('hide');
-      }
-
-      this.pjaxProgressBar_dom.style.width = '0';
-      this.pjaxProgressIcon_dom.classList.add('show');
-
-      let width = 1;
-      const maxWidth = 99;
-
-      this.pjaxProgressBar_dom.classList.add('show');
-      this.pjaxProgressBar_dom.style.width = width + '%';
-
-      this.pjaxProgressBarTimer = setInterval(() => {
-        width += 5;
-        if (width > maxWidth) width = maxWidth;
-        this.pjaxProgressBar_dom.style.width = width + '%';
-      }, 100);
-    },
-
-    // loading progress bar end
-    pjaxProgressBarEnd() {
-      this.pjaxProgressBarTimer && clearInterval(this.pjaxProgressBarTimer);
-      this.pjaxProgressBar_dom.style.width = '100%';
-
-      const temp_1 = setTimeout(() => {
-        this.pjaxProgressBar_dom.classList.remove('show');
-        this.pjaxProgressIcon_dom.classList.remove('show');
-
-        if (this.isHasScrollProgressBar) {
-          this.scrollProgressBar_dom.classList.remove('hide');
-        }
-
-        const temp_2 = setTimeout(() => {
-          this.pjaxProgressBar_dom.style.width = '0';
-          clearTimeout(temp_1), clearTimeout(temp_2);
-        }, 200);
-
-      }, 200);
+    clearTimeout(timeout)
+    if (curTime - startTime >= mustRun) {
+      func.apply(context, args)
+      startTime = curTime
+    } else {
+      timeout = setTimeout(func, wait)
     }
   }
+};
 
-  // init scroll
-  KEEP.utils.registerWindowScroll();
+function isMobile() {
+  var check = false;
+  (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true })(navigator.userAgent || navigator.vendor || window.opera)
+  return check
+};
 
-  // toggle show tools list
-  KEEP.utils.toggleShowToolsList();
+window.debounce = debounce
 
-  // global font adjust
-  KEEP.utils.globalFontAdjust();
+window.throttle = throttle
 
-  // adjust content area width
-  KEEP.utils.contentAreaWidthAdjust();
-
-  // go comment
-  KEEP.utils.goComment();
-
-  // init page height handle
-  KEEP.utils.initPageHeightHandle();
-
-  // init first screen height
-  KEEP.utils.initFirstScreenHeight();
-
-  // big image viewer handle
-  KEEP.utils.imageViewer();
-
-  // set how long age in home article block
-  KEEP.utils.setHowLongAgoInHome();
-
-}
+window.isMobile = isMobile
