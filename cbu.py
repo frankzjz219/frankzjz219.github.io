baae14c0ac6fe131922f7362db218a6eae16ab64
frankzjz <xjlzjz@163.com>
2024-04-25 16:23:27 +0800

    Site updated: 2024-04-25 16:23:25

--- cbu.py
+++
@@ -1,5 +0,0 @@
-import os 
-os.system("echo cleaning old blogs and generate new ones")
-os.system("hexo clean&&echo \\033[34;1m清除缓存中\\033[0m")
-os.system("hexo g")
-os.system("hexo d||echo \\033[31;1m********NETWORK ERROR***Please Check VPN Connection********\\033[0m")
