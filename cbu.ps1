fc6fca7faab6454dfbb22e3ed07a6b6dc4609310
frankzjz <xjlzjz@163.com>
2024-04-25 16:13:04 +0800

    Site updated: 2024-04-25 16:13:02

--- cbu.ps1
+++ cbu.ps1
@@ -1,6 +1,10 @@
 # Set the error preference to stop the script on any command failure
 $ErrorActionPreference = 'Stop'
 
+# Set the current directory to the script's root directory
+$PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+Set-Location $PSScriptRoot
+
 # Define a function to execute a command and check if it was successful
 function Invoke-HexoCommand {
     param(
